608387133ec3b5193dd13470a371d94df3d6275a
root <root@ipa-server2.womeninlinux.com>
2016-09-03 15:26:39 -0400

    new commit

--- elk-bash-install.sh
+++ elk-bash-install.sh
@@ -32,39 +32,39 @@ echo "$itsjava\n"
 
 ##########################################
 
-##Setup elasticsearch repo
-#
-#echo "Setting up repo"
-#
-##Here we are importing the key for elasticsearch
-#rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch
-#
-#
-##We naming the elasticsearch.repo which found in /etc/yum.repos.d/
-#
-#echo '[elasticsearch-2.x]' >> $elasticrepo 
-#echo "name=Elasticsearch repository for 2.x packages" >> $elasticrepo 
-#echo "baseurl=http://packages.elastic.co/elasticsearch/2.x/centos" >> $elasticrepo 
-#echo "gpgcheck=1" >> $elasticrepo
-#echo "gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch" >> $elasticrepo 
-#echo "enabled=1" >> $elasticrepo
-#
-###########################################
-#
-##now the repo has been setup we will install elastic via yum
-#
-#
-#echo "we are installing elastic"
-#
-#yum -y install elasticsearch
-#
-#esearch=`rpm -qa | grep elastic`
-#
-#echo -n "$esearch"
-#
-#sed -i '/network.host/c\network.host: localhost' /etc/elasticsearch/elasticsearch.yml
-#
-##We will now enable and start this service
-#
-#echo "We are enabled and starting elastic"
-#systemctl enable elasticsearch && systemctl start elasticsearch
+#Setup elasticsearch repo
+
+echo "Setting up repo"
+
+#Here we are importing the key for elasticsearch
+rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch
+
+
+#We naming the elasticsearch.repo which found in /etc/yum.repos.d/
+
+echo '[elasticsearch-2.x]' >> $elasticrepo 
+echo "name=Elasticsearch repository for 2.x packages" >> $elasticrepo 
+echo "baseurl=http://packages.elastic.co/elasticsearch/2.x/centos" >> $elasticrepo 
+echo "gpgcheck=1" >> $elasticrepo
+echo "gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch" >> $elasticrepo 
+echo "enabled=1" >> $elasticrepo
+
+##########################################
+
+#now the repo has been setup we will install elastic via yum
+
+
+echo "we are installing elastic"
+
+yum -y install elasticsearch
+
+esearch=`rpm -qa | grep elastic`
+
+echo -n "$esearch"
+
+sed -i '/network.host/c\network.host: localhost' /etc/elasticsearch/elasticsearch.yml
+
+#We will now enable and start this service
+
+echo "We are enabled and starting elastic"
+systemctl enable elasticsearch && systemctl start elasticsearch
