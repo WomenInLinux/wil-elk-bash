eb2b6a8aa296ad2206677512b5bd1832171314b8
root <root@elk-client.c.desktops-womeninlinux.internal>
2016-10-12 18:38:25 -0400

    Adding file with changes

--- elk-bash-joint.sh
+++ elk-bash-joint.sh
@@ -22,7 +22,7 @@ yum remove kibana elasticsearch  epel-release-latest-7.noarch.rpm jre1.8.* -y
 
 elasticrepo="/etc/yum.repos.d/elasticsearch.repo"
 kibanarepo="/etc/yum.repos.d/kibana.repo"
-ip=104.196.106.115
+ip=104.196.186.130
 
 
 #Uninstalling java
@@ -85,16 +85,22 @@ esearch=`rpm -qa | grep elastic`
 
 echo -n "$esearch"
 
-sed -i '/network.host/c\network.host: 104.196.106.115' /etc/elasticsearch/elasticsearch.yml
+sed -i '/network.host/c\ network.host: elk-client' /etc/elasticsearch/elasticsearch.yml
+sed -i '/path.data/c\ path.data: /elk' /etc/elasticsearch/elasticsearch.yml
+
+mkdir /elk
+chown elasticsearch.elasticsearch /elk
 
 #We will now enable and start this service
 
 echo "We are enabled and starting elastic"
-systemctl daemon-reload
+#systemctl daemon-reload
 systemctl enable elasticsearch 
 systemctl restart elasticsearch
 #service elasticsearch start
 #chkconfig elasticsearch on
+
+break
 
 #Created by Ayotunde
 #Create and edit a new yum repository file for Kibana.
@@ -154,12 +160,12 @@ htpasswd -b -c /etc/nginx/htpasswd.users kibanaadmin ltcuser
 
 echo 'server {
      listen 80;
-     server_name 104.196.106.115;
+     server_name 104.196.186.130;
      auth_basic "Restricted Access";
      auth_basic_user_file /etc/nginx/htpasswd.users;
 
     location / {
-        proxy_pass http://104.1096.106.115:5601;
+        proxy_pass http://104.196.186.130:5601;
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection 'upgrade';
@@ -179,94 +185,94 @@ systemctl start nginx
 systemctl enable nginx
 #setsebool -P httpd_can_network_connect 1
 
-##Installing logstash
-#echo '[logstash-2.2]
-#name=logstash repository for 2.2 packages
-#baseurl=http://packages.elasticsearch.org/logstash/2.2/centos
-#gpgcheck=1
-#gpgkey=http://packages.elasticsearch.org/GPG-KEY-elasticsearch
-#enabled=1 ' | tee /etc/yum.repos.d/logstash.repo
-#
-#yum install -y logstash
-#
-#echo 'input {
-#  beats {
-#    port => 5044
-#    ssl => true
-#    ssl_certificate => "/etc/pki/tls/certs/logstash-forwarder.crt"
-#    ssl_key => "/etc/pki/tls/private/logstash-forwarder.key"
-#  }
-#}' | tee /etc/logstash/conf.d/02-beats-input.conf 
-#
-##Logstash filters 
-#
-#cat <<EOF > /etc/logstash/conf.d/10-syslog-filter.conf
-#filter {
-#  if [type] == "syslog" {
-#    grok {
-#      match => { "message" => "%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} %{DATA:syslog_program}(?:\[%{POSINT:syslog_pid}\])?: %{GREEDYDATA:syslog_message}" }
-#    add_field => [ "received_at", "%{@timestamp}" ]
-#    add_field => [ "received_from", "%{host}" ]
-#    }
-#    syslog_pri { }
-#    date {
-#      match => [ "syslog_timestamp", "MMM  d HH:mm:ss", "MMM dd HH:mm:ss" ]
-#    }
-#  }
-#}
-#EOF
-#
-#cat <<EOF > /etc/logstash/conf.d/30-elasticsearch-output.conf
-#output {
-#  elasticsearch {
-#    hosts => ["${ip}:9200"]
-#    sniffing => true
-#    manage_template => false
-#    index => "%{[@metadata][beat]}-%{+YYYY.MM.dd}"
-#    document_type => "%{[@metadata][type]}"
-#  }
-#}
-#EOF
-#
-#cd /etc/pki/tls
-#
-#if ! grep -q "subjectAltName = IP: ${ip}"  /etc/pki/tls/openssl.cnf; then
-#  sed -i "/^\[ v3_ca \]/a subjectAltName = IP: ${ip}" /etc/pki/tls/openssl.cnf
-#fi
-#
-#sudo openssl req -config /etc/pki/tls/openssl.cnf \
-#     -x509 -days 3650 -batch -nodes -newkey rsa:2048 \
-#     -keyout private/logstash-forwarder.key -out certs/logstash-forwarder.crt
-#
-#service logstash configtest \
-# && systemctl restart logstash \
-# && sudo chkconfig logstash on
-############################################################
-##Load Kibana Dashboards
-#rm -rf /opt/beats*
-#
-#cd /opt
-#
-#curl -L -O https://download.elastic.co/beats/dashboards/beats-dashboards-1.1.0.zip
-#
-#yum -y install unzip
-#
-#unzip beats-dashboards*.zip
-#
-#cd beats-dashboards-*
-#
-#chmod +x load.sh
-#
-#sed -i '/localhost/c\http://104.196.106.115:9200/' load.sh
-#
-#sh load.sh
-#
-#cd /opt
-#
-#curl -O https://gist.githubusercontent.com/thisismitch/3429023e8438cc25b86c/raw/d8c479e2a1adcea8b1fe86570e42abab0f10f364/filebeat-index-template.json
-#
-#
-#curl -XPUT 'http://104.196.106.115:9200/_template/filebeat?pretty' -d@filebeat-index-template.json
-#
-##You need to replace this with the appropriate client
-##scp /etc/pki/tls/certs/logstash-forwarder.crt tameika@10.142.0.2:/tmp
+#Installing logstash
+echo '[logstash-2.2]
+name=logstash repository for 2.2 packages
+baseurl=http://packages.elasticsearch.org/logstash/2.2/centos
+gpgcheck=1
+gpgkey=http://packages.elasticsearch.org/GPG-KEY-elasticsearch
+enabled=1 ' | tee /etc/yum.repos.d/logstash.repo
+
+yum install -y logstash
+
+echo 'input {
+  beats {
+    port => 5044
+    ssl => true
+    ssl_certificate => "/etc/pki/tls/certs/logstash-forwarder.crt"
+    ssl_key => "/etc/pki/tls/private/logstash-forwarder.key"
+  }
+}' | tee /etc/logstash/conf.d/02-beats-input.conf 
+
+#Logstash filters 
+
+cat <<EOF > /etc/logstash/conf.d/10-syslog-filter.conf
+filter {
+  if [type] == "syslog" {
+    grok {
+      match => { "message" => "%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} %{DATA:syslog_program}(?:\[%{POSINT:syslog_pid}\])?: %{GREEDYDATA:syslog_message}" }
+    add_field => [ "received_at", "%{@timestamp}" ]
+    add_field => [ "received_from", "%{host}" ]
+    }
+    syslog_pri { }
+    date {
+      match => [ "syslog_timestamp", "MMM  d HH:mm:ss", "MMM dd HH:mm:ss" ]
+    }
+  }
+}
+EOF
+
+cat <<EOF > /etc/logstash/conf.d/30-elasticsearch-output.conf
+output {
+  elasticsearch {
+    hosts => ["${ip}:9200"]
+    sniffing => true
+    manage_template => false
+    index => "%{[@metadata][beat]}-%{+YYYY.MM.dd}"
+    document_type => "%{[@metadata][type]}"
+  }
+}
+EOF
+
+cd /etc/pki/tls
+
+if ! grep -q "subjectAltName = IP: ${ip}"  /etc/pki/tls/openssl.cnf; then
+  sed -i "/^\[ v3_ca \]/a subjectAltName = IP: ${ip}" /etc/pki/tls/openssl.cnf
+fi
+
+sudo openssl req -config /etc/pki/tls/openssl.cnf \
+     -x509 -days 3650 -batch -nodes -newkey rsa:2048 \
+     -keyout private/logstash-forwarder.key -out certs/logstash-forwarder.crt
+
+service logstash configtest \
+ && systemctl restart logstash \
+ && sudo chkconfig logstash on
+###########################################################
+#Load Kibana Dashboards
+rm -rf /opt/beats*
+
+cd /opt
+
+curl -L -O https://download.elastic.co/beats/dashboards/beats-dashboards-1.1.0.zip
+
+yum -y install unzip
+
+unzip beats-dashboards*.zip
+
+cd beats-dashboards-*
+
+chmod +x load.sh
+
+sed -i '/localhost/c\http://104.196.186.130:9200/' load.sh
+
+sh load.sh
+
+cd /opt
+
+curl -O https://gist.githubusercontent.com/thisismitch/3429023e8438cc25b86c/raw/d8c479e2a1adcea8b1fe86570e42abab0f10f364/filebeat-index-template.json
+
+
+curl -XPUT 'http://104.196.186.130:9200/_template/filebeat?pretty' -d@filebeat-index-template.json
+
+#You need to replace this with the appropriate client
+#scp /etc/pki/tls/certs/logstash-forwarder.crt tameika@10.142.0.2:/tmp
